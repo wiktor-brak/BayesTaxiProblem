325033e8617ccb0c6509c194acb82a1edb879c89
Wiktor Brak <wiktor.brak@wp.pl>
2020-05-11 12:38:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Encog.ML.Bayesian;
+using Encog.ML.Bayesian.Query.Enumeration;
+using System;
+
+namespace BayesianTaxiProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BayesianNetwork network = new BayesianNetwork();
+            BayesianEvent UberDriver = network.CreateEvent("uber_driver");
+            BayesianEvent WitnessSawUberDriver = network.CreateEvent("saw_uber_driver");
+            network.CreateDependency(UberDriver, WitnessSawUberDriver);
+            network.FinalizeStructure();
+            UberDriver?.Table?.AddLine(0.85, true);
+            WitnessSawUberDriver?.Table?.AddLine(0.80, true, true);
+            WitnessSawUberDriver?.Table?.AddLine(0.20, true, false);
+            network.Validate();
+            Console.WriteLine(network.ToString());
+            Console.WriteLine($"Parameter count: {network.CalculateParameterCount()}");
+            EnumerationQuery query = new EnumerationQuery(network);
+            query.DefineEventType(WitnessSawUberDriver, EventType.Evidence);
+            query.DefineEventType(UberDriver, EventType.Outcome);
+            query.SetEventValue(WitnessSawUberDriver, false);
+            query.SetEventValue(UberDriver, false);
+            query.Execute();
+            Console.WriteLine(query.ToString());
+        }
+    }
+}
